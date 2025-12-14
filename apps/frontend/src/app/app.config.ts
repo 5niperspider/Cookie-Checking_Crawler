f2f63018802cde1d67821932aac40084848099b2
ha75274 <145998885+ha75274@users.noreply.github.com>
2025-12-14 23:21:50 +0100

    Feature/hafsa changes (#19)
    * Erstes Exemplar der Seite
    
    * Delete apps/api/src/app/app.controller.spec.ts
    
    * Delete apps/api/src/app/app.service.spec.ts
    
    * Neue Anpassungen mit dem Api-call
    
    * import gelöscht
    
    ---------
    
    Co-authored-by: G. K. <62932392+5niperspider@users.noreply.github.com>

--- apps/frontend/src/app/app.config.ts
+++ apps/frontend/src/app/app.config.ts
@@ -7,6 +7,7 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { appRoutes } from './app.routes';
 
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
