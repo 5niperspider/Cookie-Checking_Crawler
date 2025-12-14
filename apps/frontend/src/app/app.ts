7191d653540a0a16fac9aa5ba64c7b0af99267f3
ha75274 <hafsa.kenmoesamkeu@studmail.w-hs.de>
2025-12-14 22:34:44 +0100

    Neue Anpassungen mit dem Api-call

--- apps/frontend/src/app/app.ts
+++ apps/frontend/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { SearchBar } from './pages/search-bar/search-bar';
 import { MyComponent } from "./pages/component/component";
 
 @Component({
-  imports: [SearchBar, RouterModule, MyComponent],
+  imports: [RouterModule, MyComponent],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
