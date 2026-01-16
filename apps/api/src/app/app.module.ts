f4e34ba113003b007d524c16810236df07baa550
G. K. <62932392+5niperspider@users.noreply.github.com>
2026-01-16 17:19:37 +0100

    Crawler.config (#31)
    Co-authored-by: Calderon112 <jardel.kennetedjeu@studmail.w-hs.de>

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CookiesModule } from './cookies/cookies.module';
-import { SessionsModul } from './sessions/sessions.module';
+import { SessionsModule } from './sessions/sessions.module';
 import { DbService } from './db/db.service';
 import { SchedulerService } from './scheduler/scheduler.service';
 import { CrawlerService } from './crawler.service';
@@ -8,7 +8,7 @@ import { AnalyticsService } from './analytics/analytics.service';
 import { AnalyticsController } from './analytics/analytics.controller';
 
 @Module({
-  imports: [CookiesModule, SessionsModul],
+  imports: [CookiesModule, SessionsModule],
   controllers: [AnalyticsController],
   providers: [DbService, SchedulerService, CrawlerService, AnalyticsService],
 })
